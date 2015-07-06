a7cc2523181a2324e03645bdaccc21152bc1f956
Don Clore <don.clore@scout.com>
2015-07-05 17:07:40 -0700

    master - got bank and program buttons working, radiobutton functionality happening - still need bank and program indicators

--- ProgramSwitcher/ProgramSwitcher/ProgramSwitcherViewController.swift
+++ ProgramSwitcher/ProgramSwitcher/ProgramSwitcherViewController.swift
@@ -15,95 +15,199 @@ class ProgramSwitcherViewController: UIViewController {
     
     @IBOutlet var channelButtons: [UIButton]!
     
+    @IBOutlet var bankButtons: [UIButton]!
+    
+    @IBOutlet var programButtons: [UIButton]!
+    
     var buttonImage: UIImage!
     var buttonSelectedImage: UIImage!
     
     
     @IBAction func Button2Press(sender: UIButton) {
         channelNumber.text = "2"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button1Press(sender: UIButton) {
         channelNumber.text = "1"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
 
     @IBAction func Button3Press(sender: UIButton) {
         channelNumber.text = "3"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     
     @IBAction func Button4Press(sender: UIButton) {
         channelNumber.text = "4"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button5Press(sender: UIButton) {
         channelNumber.text = "5"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     
     @IBAction func Button6Press(sender: UIButton) {
         channelNumber.text = "6"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button7Press(sender: UIButton) {
         channelNumber.text = "7"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     
     @IBAction func Button8Press(sender: UIButton) {
         channelNumber.text = "8"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button9Press(sender: UIButton) {
         channelNumber.text = "9"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button10Press(sender: UIButton) {
         channelNumber.text = "10"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button11Press(sender: UIButton) {
         channelNumber.text = "11"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     
     @IBAction func Button12Press(sender: UIButton) {
         channelNumber.text = "12"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     
     @IBAction func Button13Press(sender: UIButton) {
         channelNumber.text = "13"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     
     @IBAction func Button14Press(sender: UIButton) {
         channelNumber.text = "14"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button15Press(sender: UIButton) {
         channelNumber.text = "15"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
     
     @IBAction func Button16Press(sender: UIButton) {
         channelNumber.text = "16"
-        buttonPressed(sender)
+        buttonPressed(sender, buttonGroup:channelButtons)
     }
+    
+    
+    @IBAction func bankButton1Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton2Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    
+    @IBAction func bankButton3Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton4Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton5Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton6Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton7Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton8Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton9Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton10Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton11Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton12Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton13Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton14Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton15Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    @IBAction func bankButton16Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:bankButtons)
+    }
+    
+    
+    @IBAction func programButton1Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton2Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton3Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton4Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton5Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton6Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton7Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
+    @IBAction func programButton8Press(sender: UIButton) {
+        buttonPressed(sender, buttonGroup:programButtons)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -111,19 +215,16 @@ class ProgramSwitcherViewController: UIViewController {
         buttonImage = UIImage(named: "Button.jpg")
         buttonSelectedImage = UIImage(named: "ButtonSelected.png")
         
-/*
-     
-        for channelButton in channelButtons {
-            channelButton.setImage(buttonImage, forState: .Normal)
-        }
-*/
+        Button1Press(channelButtons[0])
+        bankButton1Press(bankButtons[0])
+        programButton1Press(programButtons[0])
     }
     
-    func buttonPressed(pressedButton: UIButton) {
+    func buttonPressed(pressedButton: UIButton, buttonGroup: [UIButton]) {
         
         pressedButton.setImage(buttonSelectedImage, forState: .Normal)
         
-        for button in channelButtons {
+        for button in buttonGroup {
             if (button !== pressedButton) {
                 button.setImage(buttonImage, forState: .Normal)
             }
