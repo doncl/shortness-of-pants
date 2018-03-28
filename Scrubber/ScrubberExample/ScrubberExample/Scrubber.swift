940631f4a20705fc14403bbf070575b0b0c27c0f
Don Clore <doncl@maven.io>
2018-03-27 17:48:40 -0700

    Scrubber - checkpoint - pretty broken

--- Scrubber/ScrubberExample/ScrubberExample/Scrubber.swift
+++ Scrubber/ScrubberExample/ScrubberExample/Scrubber.swift
@@ -42,13 +42,35 @@ class Scrubber: UIControl {
   private let scrubberTrackColor : CGColor = #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1).cgColor
  
   private let thumb : CAShapeLayer = CAShapeLayer()
+  private let thumbBorder : CAShapeLayer = CAShapeLayer()
   private let thumbX : CGFloat = -3.0
   private let thumbWidth : CGFloat = 12.0
   private let thumbHeight : CGFloat = 24.0
-
+  private var thumbYOrg : CGFloat = 0.0
  
   var startIndex : Int = 1
   var endIndex : Int = 100
+  var maxY : CGFloat = 0
+
+  
+  var trackRect : CGRect = .zero
+  
+  var thumbY : CGFloat = 0.0 {
+    didSet (oldValue) {
+      if thumbY > maxY {
+        thumbY = maxY
+      } else if thumbY < 0 {
+        thumbY = 0
+      }
+      //print("newValue = \(thumbY)")
+      let yOffset = thumbY - oldValue
+      let newPosition = CGPoint(x: thumb.position.x, y: thumbY)
+      thumb.position = newPosition
+    }
+  }
+  
+  private var initialThumbTouchY : CGFloat = 0.0
+  private var inSlide : Bool = false
   
   init() {
     super.init(frame: .zero)
@@ -85,31 +107,37 @@ class Scrubber: UIControl {
     setupTextLabel(oldest, text: "Oldest", parentRect: bottomBracket.frame)
     
     layer.addSublayer(scrubberTrack)
-    let trackRect : CGRect = CGRect(x: trackX, y: trackY, width: trackWidth,
+    trackRect = CGRect(x: trackX, y: trackY, width: trackWidth,
                                     height: height - trackDistanceFromBottom - trackY)
     
     
     let trackPath = UIBezierPath(roundedRect: trackRect, byRoundingCorners: UIRectCorner.allCorners,
                             cornerRadii: trackRadii)
     
-    
+    maxY = trackRect.maxY - (thumbHeight / 2.0)
     scrubberTrack.path = trackPath.cgPath
     scrubberTrack.fillColor = scrubberTrackColor
     
     scrubberTrack.addSublayer(thumb)
     thumb.masksToBounds = false
     
-    let thumbRect : CGRect = CGRect(x: trackRect.origin.x + thumbX , y: trackRect.origin.y,
-                                    width: thumbWidth, height: thumbHeight)
+    let thumbRect = CGRect(x: trackRect.origin.x + thumbX , y: trackRect.origin.y,
+                           width: thumbWidth, height: thumbHeight)
+    
+    thumbYOrg = thumbRect.origin.y
     
     let thumbPath = UIBezierPath(roundedRect: thumbRect,
                                  byRoundingCorners: UIRectCorner.allCorners,
                                  cornerRadii: thumbRadii)
     
-    thumb.borderColor = thumbBorderColor
+    thumb.zPosition = 0.0
+    thumb.strokeColor = thumbBorderColor
+    thumb.lineWidth = thumbBorderWidth
     thumb.fillColor = UIColor.white.cgColor
-    thumb.borderWidth = thumbBorderWidth
     thumb.path = thumbPath.cgPath
+    isUserInteractionEnabled = true
+    isMultipleTouchEnabled = false
+    
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -120,6 +148,72 @@ class Scrubber: UIControl {
     get {
       return CGSize(width: width, height: height)
     }
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let location = locationOfFirstTouch(inTouches: touches) else {
+      return
+    }
+    if hitTest(location) {
+      initialThumbTouchY = location.y
+      inSlide = true
+      print("inSlide = true")
+    }
+  }
+  
+  private func hitTest(_ point : CGPoint) -> Bool {
+    guard let path = thumb.path else {
+      return false
+    }
+    let adjustedRect = path.boundingBoxOfPath.offsetBy(dx: 0.0, dy: thumb.position.y)
+    let ret = adjustedRect.contains(point)
+    print("hitTest returns \(ret)")
+    return ret
+  }
+  
+  fileprivate func calculateNewThumbY(_ location: CGPoint) {
+    let deltaY = location.y - initialThumbTouchY
+//    print("deltaY = \(deltaY)")
+    thumbY = thumbYOrg + deltaY
+  }
+  
+  override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard inSlide else {
+      return
+    }
+    guard let location = locationOfFirstTouch(inTouches: touches) else {
+      return
+    }
+    calculateNewThumbY(location)
+  }
+  
+  
+  
+  override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    defer {
+      initialThumbTouchY = 0.0
+      inSlide = false
+    }
+    
+    guard inSlide else {
+      return
+    }
+
+    guard let location = locationOfFirstTouch(inTouches: touches) else {
+      print("touchesEnded - couldn't get location")
+      return
+    }
+    
+    print("location = \(location.y)")
+    calculateNewThumbY(location)
+  }
+  
+  private func locationOfFirstTouch(inTouches touches : Set<UITouch>) -> CGPoint? {
+      
+      guard let touch = touches.first else {
+        return nil
+      }
+      return touch.location(in: touch.view)
   }
   
   private func setupTextLabel(_ label: UILabel, text: String, parentRect : CGRect) {
